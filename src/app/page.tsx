328c38fd1b75c186d9bbc49c5a16842605277f88
Vardan <vardan.vanyan@gmail.com>
2023-04-26 17:33:20 -0700

    getting pokemon data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,40 @@
+'use client';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
+import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <h1>Hello World</h1>;
+  const [pokemonList, setPokemonList] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?limit=151`
+      );
+      const data = await response.json();
+      const promises = data.results.map((pokemon: object) => {
+        return fetch(pokemon.url).then((res) => res.json());
+      });
+      Promise.all(promises).then((results) => {
+        setPokemonList(results);
+        setLoading(false);
+      });
+    }
+    fetchData();
+  }, []);
+
+  if (loading) return <h1>Loading....</h1>;
+  console.log(pokemonList);
+  return (
+    <div>
+      {/* {pokemonList.map((pokemon: object, index: number) => (
+        <div key={index}>
+          <h1>{pokemon.name}</h1>
+        </div>
+      ))} */}
+    </div>
+  );
 }
