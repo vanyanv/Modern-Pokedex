4c9d721406603d5b05e3c6b26f1256ca7bce67e1
Vardan <vardan.vanyan@gmail.com>
2023-04-29 15:59:41 -0700

    added nexted routes for routing and made changes to nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,19 +4,20 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Link } from 'react-router-dom';
 import Image from 'next/image';
 import pikachuRunning from '../../public/pikachuRunning.gif';
+import pokeball from '../../public/pokeball.gif';
 const navigation = [{ name: 'Home', href: '/' }];
 
 export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className='bg-white'>
+    <header className='bg-white border'>
       <nav
         className='mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8'
         aria-label='Global'
       >
         <div className='flex lg:flex-1'>
-          <a href='#' className='-m-1.5 p-1.5'>
+          <Link to='/' className='-m-1.5 p-1.5'>
             <span className='sr-only'>Your Company</span>
             <Image
               className='h-12 w-auto'
@@ -25,7 +26,7 @@ export default function Example() {
               width={8}
               height={8}
             />
-          </a>
+          </Link>
         </div>
         <div className='hidden lg:flex lg:gap-x-12'>
           {navigation.map((item) => (
@@ -46,12 +47,17 @@ export default function Example() {
           >
             Log in
           </a>
-          <a
-            href='#'
-            className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-          >
-            Sign up
-          </a>
+
+          <div className='flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
+            <Image
+              className='h-8 w-auto '
+              src={pokeball}
+              alt='pokeball'
+              width={8}
+              height={8}
+            />
+            <a>Sign up</a>
+          </div>
         </div>
         <div className='flex lg:hidden'>
           <button
@@ -83,12 +89,16 @@ export default function Example() {
                 height={8}
               />
             </a>
-            <a
-              href='#'
-              className='ml-auto rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-            >
-              Sign up
-            </a>
+            <div className=' flex items-center ml-auto rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
+              <Image
+                className='h-8 w-auto '
+                src={pokeball}
+                alt='pokeball'
+                width={8}
+                height={8}
+              />
+              <a>Sign up</a>
+            </div>
             <button
               type='button'
               className='-m-2.5 rounded-md p-2.5 text-gray-700'
